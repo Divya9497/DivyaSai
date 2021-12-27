b8b4cda82e4ee0853b76ddc414e9f79ef2d47b3d
Yaswanth Kondeti <divyash@Yaswanths-MacBook-Air.local>
2021-12-27 22:53:46 +0530

    Adding Basic Files of Golang

---
+++ Chapter 1/02_Declaration_of_Variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var (
+	x int    = 123
+	y string = "dsp"
+	z bool   = true
+)
+
+func main() {
+	fmt.Println(x, y, z)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}

6c45fe04f32889cdadad0631ca0738d1cf1d8874
Yaswanth Kondeti <divyash@Yaswanths-MacBook-Air.local>
2021-12-28 18:20:05 +0530

    Adding 2 new files under chapter 1

---
+++ Chapter 1/03_print_type_vars.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	const f = "%T(%v)"
+	x := 42
+	y := "Divya Sai"
+	z := true
+
+	s := fmt.Sprintf("%d\t%s\t%t", x, y, z)
+	r := fmt.Sprintf("%v score is %v", y, x)
+
+	fmt.Println(s)
+	fmt.Println(r)
+	fmt.Printf(f, x, x)
+}

7d99bff61e0a7f2f50d6be3fc33882b6c599665a
Divya Sai <pdivyasai333@gmail.com>
2022-01-10 20:14:03 +0530

    Added Maps code

---
+++ Chapter4/08_Map.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := map[string][]string{
+		`bond_james`:      []string{`Shaken, not stirred`, `Martinis`, `Women`},
+		`moneypenny_miss`: []string{`James Bond`, `Literature`, `Computer Science`},
+		`no_dr`:           []string{`Being evil`, `Ice cream`, `Sunsets`},
+	}
+	//fmt.Println(m)
+	for k, v := range m {
+		fmt.Println("This is the data for", k)
+		for i, v1 := range v {
+			fmt.Println("\t", i, v1)
+		}
+	}
+}
